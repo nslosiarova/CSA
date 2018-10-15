b380681d7aa814b2ffc78241d724a4a8951f3fc3
Natália Slosiarová <natalia.slosiarova@student.leaf.academy>
2018-10-15 10:19:26 +0200

    practice on the lesson

--- unit1/armstrongNumber.java
+++ unit1/armstrongNumber.java
@@ -1,18 +1,11 @@
+import javax.swing.JOptionPane;
+import java.io.*;
+
 public class armstrongNumber{
     public static void main (String args[]){
-        int number = 420;
-        while (number > 0) {
-            System.out.println(number % 10);
-            number = number / 10;
+       
+        String input = JOptionPane.showInputDialog(null, "Let's find out if your number is the Armstrong number!"/n "Write a number.");
+        int numberOfDigits = input.length();
         
-        }
-        int a;
-        int b =4;
-        a = (++b - 2) % 3;
-        System.out.println(a);
-
-        boolean c;
-        c = (b + 9!=8) && !(false);
-        System.out.print(c);
     }
 }
---
+++ unit1/test.java
@@ -0,0 +1,7 @@
+public class test extends ConsoleProgram{
+    public static void main (String[] args){
+        System.out.println("Javapoint");
+        String i = readLine("Enter some text: ");
+        System.out.print(i);
+    }
+}
